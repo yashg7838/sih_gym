99ac3d562d215b432a31aed724de2c2a200f3246
yash <yashg7838@gmail.com>
2023-09-14 17:04:20 +0530

    push

--- lib/main.dart
+++ lib/main.dart
@@ -23,7 +23,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Color(0xFF2D2727)),
+        colorScheme: ColorScheme.fromSeed(seedColor: const Color(0xFF2D2727)),
         useMaterial3: true,
       ),
       home: MainPage(),
